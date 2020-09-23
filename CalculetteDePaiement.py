4f7091333891f2f81669ddfe79d40ae10e0cbe53
denisdoryan <71377447+denisdoryan@users.noreply.github.com>
2020-09-23 12:07:20 +0200

    Update CalculetteDePaiement.py

--- CalculetteDePaiement.py
+++ CalculetteDePaiement.py
@@ -2,3 +2,18 @@
 x = max(0.021*solde,10)
 paiMin = min(x, solde)
 print(paiMin)
+
+﻿solde = 600
+x = max(0.021*solde,10)
+paiMin = min(x, solde)
+print(paiMin)
+
+﻿solde = 25
+x = max(0.021*solde,10)
+paiMin = min(x, solde)
+print(paiMin)
+
+﻿solde = 8
+x = max(0.021*solde,10)
+paiMin = min(x, solde)
+print(paiMin)
